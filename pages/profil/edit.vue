1439f967ef14786558595c45d9943617e6fac9e0
Hashs7 <seb_her@hotmail.fr>
2021-06-08 22:00:47 +0200

    Edit profile thumbnail with preview

--- pages/profil/edit.vue
+++ pages/profil/edit.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="o-page o-page--profile-edit">
+  <form ref="form" class="o-page o-page--profile-edit" @submit="submit">
     <div class="user__header">
       <ButtonBack light class="user-back" />
       <div v-if="isOwner" class="owner">
-        <NuxtLink :to="userLink" class="btn-edit fill">Enregistrer</NuxtLink>
+        <button class="btn-edit fill">Enregistrer</button>
       </div>
     </div>
     <div class="user-banner">
@@ -11,44 +11,57 @@
     </div>
     <div class="user-infos">
       <div v-if="user.thumbnail" class="user-thumbnail">
-        <img
-          :src="user.thumbnail.path"
-          :alt="`Photo de profil de ${user.firstName}`"
-        />
+        <div class="user-thumbnail__container">
+          <input
+            ref="file"
+            class="u-button__input-file"
+            type="file"
+            accept="image/*"
+            style="opacity: 0"
+            @change="previewImg"
+          />
+          <IconEdit class="edit-icon" />
+          <img :src="thumbnail" :alt="`Photo de profil de ${user.firstName}`" />
+        </div>
       </div>
       <div class="o-page__container">
         <div class="user-name">
           <input
             v-model="user.firstName"
+            name="firstName"
             type="text"
             class="editable-input user-firstname"
           />
           <input
             v-model="user.lastName"
+            name="lastName"
             type="text"
             class="editable-input user-lastname"
           />
         </div>
         <p class="user-username">
-          <input
+          @<input
             v-model="user.username"
             type="text"
+            name="username"
             class="editable-input user-username"
           />
         </p>
       </div>
     </div>
-  </div>
+  </form>
 </template>
 
 <script>
 import ButtonBack from '../../components/UI/ButtonBack';
+import IconEdit from '~/assets/svg/ic_edit.svg?inline';
 
 export default {
-  components: { ButtonBack },
+  components: { ButtonBack, IconEdit },
   middleware: 'auth',
   data() {
     return {
+      thumbnailPreview: null,
       user: {
         firstName: null,
         lastName: null,
@@ -74,11 +87,48 @@ export default {
     isOwner() {
       return this.$auth.$state.user._id === this.user._id;
     },
+    thumbnail() {
+      if (this.thumbnailPreview) {
+        return this.thumbnailPreview;
+      }
+      return this.user.thumbnail.path;
+    },
   },
   mounted() {
     if (this.$auth.loggedIn) {
       this.user = { ...this.$auth.$state.user };
     }
+  },
+  methods: {
+    async submit(e) {
+      e.preventDefault();
+      const formData = new FormData(this.$refs.form);
+      const file = this.$refs.file.files[0];
+      if (file) {
+        formData.append('thumbnail', file);
+      }
+      try {
+        await this.$api.updateUser(formData);
+        await this.$auth.fetchUser();
+        this.redirect();
+      } catch (e) {
+        this.notifyError();
+      }
+    },
+
+    redirect() {
+      this.$router.push(this.userLink);
+    },
+
+    previewImg() {
+      const fileReader = new FileReader();
+      const f = this.$refs.file.files[0];
+      console.log(f);
+      fileReader.readAsDataURL(f);
+      fileReader.addEventListener('loadend', (e) => {
+        this.thumbnailPreview = fileReader.result;
+      });
+    },
   },
 };
 </script>
@@ -92,10 +142,21 @@ export default {
   .user-name {
     font-size: 0;
   }
+  .user-thumbnail__container {
+    position: relative;
+    border-radius: 50%;
+    height: 100%;
+    overflow: hidden;
+  }
+  .user-username input {
+    min-width: 100px;
+    text-align: left;
+    padding-left: 8px;
+  }
 
   .user-firstname {
     display: inline-block;
-    text-align: right;
+    //text-align: right;
     margin-right: 4px;
     width: calc(50% - 4px);
     padding: 8px 4px;
@@ -105,7 +166,30 @@ export default {
     margin-left: 4px;
     width: calc(50% - 4px);
     padding: 8px 4px;
-    text-align: left;
+    //text-align: left;
+  }
+
+  .user-thumbnail__container {
+    &:after {
+      content: '';
+      z-index: 4;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      background-color: rgba($gray-darkest, 0.5);
+    }
+  }
+
+  .edit-icon {
+    z-index: 5;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
   }
 }
 </style>
