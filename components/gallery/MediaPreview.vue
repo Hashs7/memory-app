3bb3667da95377627e9e46b6eec332fcaf7435f0
Hashs7 <seb_her@hotmail.fr>
2021-06-11 09:33:26 +0200

    Audio preview visualizer

--- components/gallery/MediaPreview.vue
+++ components/gallery/MediaPreview.vue
@@ -4,10 +4,13 @@
       <ButtonBack emit @back="close" />
       <div class="o-page__outside">
         <img v-if="isImage" class="" :src="media.path" alt="" />
-        <audio v-if="isAudio" controls>
-          <source :src="media.path" :type="media.mimetype" />
-          Sorry, your browser doesn't support embedded videos.
-        </audio>
+        <div v-if="isAudio" class="">
+          <canvas id="canvas" class="audio-vizualiser"></canvas>
+          <audio id="audio-element" controls>
+            <source :src="media.path" :type="media.mimetype" />
+            Sorry, your browser doesn't support embedded videos.
+          </audio>
+        </div>
         <video v-if="isVideo" controls>
           <source :src="media.path" :type="media.mimetype" />
           Sorry, your browser doesn't support embedded videos.
@@ -26,6 +29,7 @@
 </template>
 
 <script>
+import Wave from '@foobar404/wave';
 import IconTrash from '@/assets/svg/ic_trash.svg?inline';
 import IconDownload from '@/assets/svg/ic_download.svg?inline';
 import ButtonBack from '../UI/ButtonBack';
@@ -43,6 +47,11 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      wave: null,
+    };
+  },
   computed: {
     downloadUrl() {
       return `${this.media.path}?download=true`;
@@ -57,6 +66,10 @@ export default {
       return this.media.mimetype.split('/')[0] === 'audio';
     },
   },
+  mounted() {
+    if (!this.isAudio) return;
+    this.enableAudio();
+  },
   methods: {
     close() {
       this.$store.commit('gallery/setPreview', null);
@@ -66,8 +79,19 @@ export default {
         await this.$store.dispatch('gallery/deleteMedia', this.media._id);
         this.close();
       } catch (e) {
-        throw new Error(e);
+        console.log(e);
       }
+    },
+    enableAudio() {
+      const canvas = document.getElementById('canvas');
+      canvas.width = window.innerWidth;
+      canvas.height = (9 / 16) * window.innerWidth;
+
+      this.wave = new Wave();
+      this.wave.fromElement('audio-element', 'canvas', {
+        type: 'dualbars',
+        colors: ['#373737', '#373737'],
+      });
     },
   },
 };
@@ -83,5 +107,9 @@ export default {
   position: absolute;
   bottom: 20px;
   right: 0;
+}
+.audio-vizualiser {
+  display: block;
+  width: 100%;
 }
 </style>
